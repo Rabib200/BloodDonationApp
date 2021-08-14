c7263e0bfde321d890c741f2f73be6bbda0b9372
Rabib <rabibhaque200@gmail.com>
2021-08-15 02:24:35 +0600

    0. initiate server

---
+++ data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        name : "hasan",
+        age : 10 ,
+        bloodGroup: "b+"
+    },
+    {
+        name : "Rabib",
+        age : 21,
+        bloodGroup: "B+"
+    },
+    {
+        name : "Tasrif",
+        age : 20,
+        bloodGroup: "O-"
+    },
+];
---
+++ index.js
@@ -0,0 +1,44 @@
+import express, { request, response } from "express";
+import { data } from "./data.js";
+
+
+const app = express();
+
+
+app.get('/', (request, response) => {
+    return response.status(200).send(
+        {
+            name: "First Server"
+        }
+    );
+}
+
+
+
+
+
+);
+
+app.get("/donors", (req, res) => {
+    return res.status(200).send(data);
+})
+//Serve
+
+app.listen(5000, console.log("server runnning at http://localhost:5000"));
+//REST API METHODS
+//GET - get data
+//POST - create one data
+//PUT - update one data
+//delete
+
+
+//Request 
+//Header
+//Body
+//URI
+//Method
+
+// Response
+//Header
+//Body
+//status code
